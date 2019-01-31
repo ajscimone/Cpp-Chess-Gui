7a6b41f67238790886422365343f18094246ca0d
Alex Johnson <alxmjo@gmail.com>
2019-01-31 14:43:59 -0800

    Remove old tests

--- test/BoardTest.cpp
+++ test/BoardTest.cpp
@@ -4,23 +4,6 @@
 #include "../include/catch.hpp"
 #include "../source/Board.hpp"
 
-TEST_CASE("Coord and Element Conversion")
-{
-	Board board;
-
-	REQUIRE(board.coordsToElement("a1") == 56);
-	REQUIRE(board.coordsToElement("a8") == 0);
-	REQUIRE(board.coordsToElement("h1") == 63);
-	REQUIRE(board.coordsToElement("h8") == 7);
-	REQUIRE(board.coordsToElement("c6") == 18);
-
-	REQUIRE(board.elementToCoords(56) == "a1");
-	REQUIRE(board.elementToCoords(0) == "a8");
-	REQUIRE(board.elementToCoords(63) == "h1");
-	REQUIRE(board.elementToCoords(7) == "h8");
-	REQUIRE(board.elementToCoords(18) == "c6");
-}
-
 TEST_CASE("Set and Get Pieces")
 {
 	Board board;
